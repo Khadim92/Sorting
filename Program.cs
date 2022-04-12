ea741e6cc4e5352dbbb99137daa924705a37d55f
Khadim92 <imkhadim1@gmail.com>
2022-04-12 09:27:38 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,5,4};
+            List<int> l2=list.OrderByDescending(x => x).ToList();
+            foreach (int x in l2)
+            {
+                Console.WriteLine(x+" ");
+            }
+        }
+    }
+}
